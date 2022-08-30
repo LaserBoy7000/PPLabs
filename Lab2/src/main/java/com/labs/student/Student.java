329f20c2a84a191334769b9321321760f1170841
LaserBoy7000 <vasilyis2004@gmail.com>
2022-08-30 10:55:18 +0300

    Student class implemented

--- Lab2/src/main/java/com/labs/student/Student.java
+++ Lab2/src/main/java/com/labs/student/Student.java
@@ -1,5 +1,62 @@
 package com.labs.student;
 
+import java.sql.Date;
+
 public class Student {
+    int ID;
+    String Surname;
+    String Name;
+    Date Birthday;
+    String Adress;
+    int Phone;
+    String Faculty;
+    int Course;
+    int Group;
     
+    public Student(int iD, String surname, String name, Date birthday, String adress, int phone, String faculty,
+            int course, int group) {
+        ID = iD;
+        Surname = surname;
+        Name = name;
+        Birthday = birthday;
+        Adress = adress;
+        Phone = phone;
+        Faculty = faculty;
+        Course = course;
+        Group = group;
+    }
+    
+    //
+    public String getFaculty() {
+        return Faculty;
+    }
+    public void setFaculty(String faculty) {
+        Faculty = faculty;
+    }
+    //
+    //
+    public Date getBirthday() {
+        return Birthday;
+    }
+    //
+    //
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    //
+    //
+    public String getSurname() {
+        return Surname;
+    }
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+    //
+    //
+    public int getID() {
+        return ID;
+    }
 }
