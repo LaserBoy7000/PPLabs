abafeb3974030f9c8389f2dbb4888ac9a7a40384
LaserBoy7000 <vasilyis2004@gmail.com>
2022-09-09 22:20:33 +0300

    Create Main class

--- Lab3/src/main/java/com/labs/Main.java
+++ Lab3/src/main/java/com/labs/Main.java
@@ -1,9 +1,5 @@
 package com.labs;
-import java.lang.reflect.Array;
-import java.nio.Buffer;
-
-import com.labs.IO.UserIO;
-
+import com.labs.Game.Game;
 /**
  * Main class
  *
@@ -12,6 +8,6 @@ public class Main
 {
     public static void main( String[] args )
     {
-      
+       Game game = new Game();
     }
 }
